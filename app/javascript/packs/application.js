e56ad84a67827ce78b5f82321d38fc84015a2633
Ali Raza <aliraza.yasin.714@gmail.com>
2021-10-13 22:34:14 +0500

    Town Management System

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require toastr
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -11,6 +12,7 @@ import "channels"
 import "jquery"
 import "@popperjs/core"
 import "bootstrap"
+global.toastr = require('toastr')
 
 Rails.start()
 Turbolinks.start()
